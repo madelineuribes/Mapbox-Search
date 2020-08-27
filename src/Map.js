72eeaa7314977548f19481397ba506e06a2d15bf
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-27 17:00:24 -0400

    Adding Navigation Control

--- src/Map.js
+++ src/Map.js
@@ -8,10 +8,19 @@ class Map extends Component {
   componentDidMount() {
     mapbox.accessToken = 'pk.eyJ1IjoibWFkZWxpbmV1cmliZXMiLCJhIjoiY2tlZDd0Z2pvMDMxMTJ5bXo2eXZiNTY4ayJ9.rfyeR7RCqUG9tv4KGHfSSg';
 
-    var map = new mapbox.Map({
+    const map = new mapbox.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [
+        -74.0060,
+        40.7128
+      ],
+      zoom: 12
     });
+
+    const navigationControl = new mapbox.NavigationControl()
+
+    map.addControl(navigationControl)
   }
 
   render() {
